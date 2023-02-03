cbb592667b747d5cf01bfdbd27b80bb15cbf96e7
BernhardPoppe <bernhard.poppe@gmail.com>
2023-02-03 10:35:42 +0100

    Matomo

--- base-theme/src/index.js
+++ base-theme/src/index.js
@@ -3,4 +3,11 @@ require('./js/_custom-vh.js')
 
 document.addEventListener('DOMContentLoaded', function() {
 	console.log('DOM Content Loaded')
+
+	    //Matomo
+        /*barba.hooks.after(() => {
+            _paq.push(['setCustomUrl', location.href]);
+            _paq.push(['setDocumentTitle', document.title]);
+            _paq.push(['trackPageView']);
+        });*/
 })
